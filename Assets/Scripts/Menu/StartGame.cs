07320946ffe103d552f82b256d3616c349fae7e0
Michael Griffiths <michaelgriffithsaus@gmail.com>
2020-02-21 21:15:57 +1100

    Added Scene switching from menu to game
    Also removed the placeholder as the folder now has legit content

---
+++ Assets/Scripts/Menu/StartGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class StartGame : MonoBehaviour
+{
+
+    //Set a default scene name that can be overriden in the console if needed
+    public string sceneName = "GameScene";
+
+
+    public void OnClick()
+    {
+        ChangeScene();
+    }
+
+    private void ChangeScene()
+    {
+        Debug.Log("Opening Scene: " + sceneName);
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
