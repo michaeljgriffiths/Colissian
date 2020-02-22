8f88d67c87a550a107751eb03d885c437420ef69
Michael Griffiths <michaelgriffithsaus@gmail.com>
2020-02-22 16:41:28 +1100

    Split out scenes added background

---
+++ Assets/Scripts/Background/Background.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+
+    public GameObject backgroundPlane;
+    private Transform backgroundTransform;
+
+    private void Start()
+    {
+        backgroundTransform = backgroundPlane.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        backgroundTransform.position = new Vector3(backgroundTransform.position.x - 1 * 2 * Time.deltaTime, backgroundTransform.position.y, 0);
+        
+    }
+}
