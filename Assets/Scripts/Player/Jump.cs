3e87f4470fc2387b1749d445ae791515f43efb55
James Cotter <jamesc@performio.co>
2020-02-22 14:12:12 +1100

    jc

---
+++ Assets/Scripts/Player/Jump.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+	public int forceConst = 10;
+	public bool grounded = true;
+
+	private new Rigidbody rigidbody;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (grounded)
+		{
+			if (Input.GetKeyUp(KeyCode.Space))
+			{
+				rigidbody.AddForce(0, forceConst, 0, ForceMode.Impulse);
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("Ground"))
+		{
+			grounded = true;
+		}
+	}
+
+	void OnCollisionExit(Collision other)
+	{
+		if (other.gameObject.CompareTag("Ground"))
+		{
+			grounded = false;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Colissian
+A high paced run and gun game
+
+
+
+# Project Setup
+
+1. Clone the repo
+2. Open Unity hub
+3. In the Projects tab select "Add"
+4. Select the Colissian folder
+5. The project *should* now appear in the list of available unity projects
