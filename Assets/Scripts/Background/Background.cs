d56c37763fad6efe8e5b526f7f3feba4463cb0b4
Michael Griffiths <michaelgriffithsaus@gmail.com>
2020-02-22 18:52:42 +1100

    still going

--- Assets/Scripts/Background/Background.cs
+++ Assets/Scripts/Background/Background.cs
@@ -8,16 +8,49 @@ public class Background : MonoBehaviour
     public GameObject backgroundPlane;
     private Transform backgroundTransform;
     public float speed;
+    public GameObject gameCameraObject;
+    private Camera gameCamera;
+    private Transform cameraTransform;
+    private int planeLength;
+    public float choke = 0;
+
+    Vector3 screenBounds;
 
     private void Start()
     {
+        gameCamera = gameCameraObject.GetComponent<Camera>();
         backgroundTransform = backgroundPlane.GetComponent<Transform>();
+        cameraTransform = gameCameraObject.GetComponent<Transform>();
+        screenBounds = gameCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, cameraTransform.position.z));
+        Debug.Log(screenBounds.ToString());
+
+        loadChildObjects(backgroundPlane);
     }
 
-    // Update is called once per frame
-    void Update()
+    void loadChildObjects(GameObject obj)
     {
-        backgroundTransform.position = new Vector3(backgroundTransform.position.x - 1 * speed * Time.deltaTime, backgroundTransform.position.y, 0);
-        
+        float objectWidth = obj.GetComponent<MeshRenderer>().bounds.size.x - choke;
+        Debug.Log("Object width is : " + objectWidth.ToString());
+        int childsNeeded = (int)Mathf.Ceil(screenBounds.x * 2 / objectWidth);
+        Debug.Log("Objects needed is " + childsNeeded.ToString());
+
+        GameObject clone = Instantiate(obj) as GameObject;
+        //for (int i = 0; i <= childsNeeded; i++)
+        {
+            //GameObject c = Instantiate(clone) as GameObject;
+            //c.transform.SetParent(obj.transform);
+            //c.transform.position = new Vector3(objectWidth * i, obj.transform.position.y, obj.transform.position.z);
+            //c.name = obj.name + i;
+        }
+        //Destroy(clone);
+        //Destroy(obj.GetComponent<MeshRenderer>());
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            backgroundTransform.position = new Vector3(backgroundTransform.position.x - 1 * speed * Time.deltaTime, backgroundTransform.position.y, 0);
+
+        }
     }
 }
