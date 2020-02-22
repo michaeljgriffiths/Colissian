dbb83655eaa2136bf82eac5a4501267fee99ac0b
James Cotter <jamesc@performio.co>
2020-02-22 14:44:34 +1100

    Actual commit times

---
+++ Assets/Scripts/Player/Move.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private int speed = 10;
+
+    public void Update()
+    {
+        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += Movement * speed * Time.deltaTime;
+    }
+}
